f9e5142477dcd1c7b78d7097d46e5a42afa7bc3e
Nutthapong Kanna <nutthapong.ka@bdi.or.th>
2025-12-03 00:44:04 +0700

    second commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-
+set -e
+echo "Deploy airflow"
 helm upgrade --install airflow apache-airflow/airflow \
   --namespace airflow \
-  -f charts/airflow/values.yaml
+  -f ../chart/airflow/values.yaml 
 
+echo "🚀 Deploying JupyterHub..."
 helm upgrade --install jhub jupyterhub/jupyterhub \
   --namespace jhub \
-  -f charts/jupyter/values.yaml
+  --create-namespace \
+  -f ../chart/jupyter/values.yaml
+
+echo "🎉 Done!"
